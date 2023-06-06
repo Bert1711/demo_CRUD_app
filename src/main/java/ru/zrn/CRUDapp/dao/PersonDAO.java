a9d8f7da21334c6cc9cf2cae9cdb1954dc816571
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-06-06 14:48:18 +0300

    "implemented the "index" method in the Person class"

--- src/main/java/ru/zrn/CRUDapp/dao/PersonDAO.java
+++ src/main/java/ru/zrn/CRUDapp/dao/PersonDAO.java
@@ -3,20 +3,51 @@ package ru.zrn.CRUDapp.dao;
 import org.springframework.stereotype.Component;
 import ru.zrn.CRUDapp.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Bertik", 32, "bertik@mail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Derev", 35, "derev@mail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Rusik", 30, "rusik@mail.com"));
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()) {
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return people;
     }
     public Person show(int id) {
