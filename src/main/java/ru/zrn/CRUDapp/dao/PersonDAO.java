77a37397b73e21a56a212c34c5c1d830085918a6
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-06-07 20:21:50 +0300

    "change 'show', 'save' and 'update' methods from PersonDAO class "

--- src/main/java/ru/zrn/CRUDapp/dao/PersonDAO.java
+++ src/main/java/ru/zrn/CRUDapp/dao/PersonDAO.java
@@ -8,9 +8,10 @@ import ru.zrn.CRUDapp.models.Person;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Component
 public class PersonDAO {
-   private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
     public PersonDAO(JdbcTemplate jdbcTemplate) {
@@ -18,22 +19,30 @@ public class PersonDAO {
     }
 
     public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>());
+        return jdbcTemplate.query(
+                "SELECT * FROM Person",
+                new BeanPropertyRowMapper<>(Person.class));
     }
+
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null );
+        return jdbcTemplate.query(
+                "SELECT * FROM Person WHERE id=?",
+                new Object[]{id},
+                new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        jdbcTemplate.update(
+                "INSERT INTO person VALUES(1, ?, ?, ?)",
+                person.getName(), person.getAge(), person.getEmail());
     }
 
     public void update(int id, Person updatePerson) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatePerson.getName());
-        personToBeUpdated.setAge(updatePerson.getAge());
-        personToBeUpdated.setEmail(updatePerson.getEmail());
+        jdbcTemplate.update(
+                "UPDATE Person SET name=?, age=?, email=? WHERE id=?",
+                updatePerson.getName(), updatePerson.getAge(), updatePerson.getEmail(), id);
+
     }
 
     public void delete(int id) {
