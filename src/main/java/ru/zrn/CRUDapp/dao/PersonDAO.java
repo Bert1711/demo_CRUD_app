edb41ce8a9333d8dcb744be47a2a5fa08ff4a3b0
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-06-07 19:54:06 +0300

    "change 'index' method from PersonDAO class "

--- src/main/java/ru/zrn/CRUDapp/dao/PersonDAO.java
+++ src/main/java/ru/zrn/CRUDapp/dao/PersonDAO.java
@@ -1,5 +1,8 @@
 package ru.zrn.CRUDapp.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.zrn.CRUDapp.models.Person;
 
@@ -7,17 +10,15 @@ import java.util.ArrayList;
 import java.util.List;
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
-    private List<Person> people;
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Bertik", 32, "bertik@mail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Derev", 35, "derev@mail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Rusik", 30, "rusik@mail.com"));
+   private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<Person> index() {
-        return people;
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>());
     }
     public Person show(int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null );
